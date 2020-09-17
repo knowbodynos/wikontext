5d6988614acc5bbf2018510df3d272474736c780
Ross Altman <knowbodynos@gmail.com>
2020-09-17 00:06:23 -0400

    Allow redeploy existing certificates

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp /etc/nginx/sites-available/wikontext.us.conf /etc/nginx/sites-enabled/
-certbot certonly --nginx -n -d wikontext.us -d www.wikontext.us
+certbot --nginx -n -d wikontext.us -d www.wikontext.us
 service nginx restart
 gunicorn --bind 0.0.0.0:8000 --timeout 60 wikontext.wsgi:app
