c9055878ef9aea8bf0864fe0b0c1ea6d677a0697
Ross Altman <knowbodynos@gmail.com>
2020-09-17 00:08:50 -0400

    Start nginx service before certbot command

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cp /etc/nginx/sites-available/wikontext.us.conf /etc/nginx/sites-enabled/
+
+service nginx start
 certbot --nginx -n -d wikontext.us -d www.wikontext.us
 service nginx restart
+
 gunicorn --bind 0.0.0.0:8000 --timeout 60 wikontext.wsgi:app
